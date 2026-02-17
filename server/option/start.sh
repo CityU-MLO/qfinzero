be9b449b15ce9c1d5e5b4cd82a3efd4bbd435c36
HAOCHEN LUO <47201049+chester1uo@users.noreply.github.com>
2026-02-17 09:44:35 +0800

    Delete server directory

--- server/option/start.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p logs
-
-nohup uvicorn daily_backtest:app --host 0.0.0.0 --port 19747 --reload \
-  > logs/daily_backtest.out 2>&1 & echo $! > logs/daily_backtest.pid
-
-nohup uvicorn app:app --host 0.0.0.0 --port 19787 --reload \
-  > logs/app.out 2>&1 & echo $! > logs/app.pid
